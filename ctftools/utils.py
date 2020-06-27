56e145b9863c3c928412b9000eda91e765c2e476
d2verb <licprog73@gmail.com>
2020-06-27 17:49:01 +0900

    change b64enc/b64dec arguments name

--- ctftools/utils.py
+++ ctftools/utils.py
@@ -1,9 +1,9 @@
-from base64 import b64encode, b64decode
+from base64 import b64decode, b64encode
 
 
-def b64enc(s: bytes) -> bytes:
-    return b64encode(s)
+def b64enc(bs: bytes) -> bytes:
+    return b64encode(bs)
 
 
-def b64dec(s: bytes) -> bytes:
-    return b64decode(s)
+def b64dec(bs: bytes) -> bytes:
+    return b64decode(bs)
