17820e85f861daf9886967a6400935e83ffbdc57
d2verb <licprog73@gmail.com>
2020-07-02 00:14:45 +0900

    implement urlenc urldec

--- ctftools/utils.py
+++ ctftools/utils.py
@@ -1,4 +1,6 @@
 from base64 import b64decode, b64encode
+from typing import Union
+from urllib.parse import quote, unquote
 
 
 def b64enc(bs: bytes) -> bytes:
@@ -7,3 +9,11 @@ def b64enc(bs: bytes) -> bytes:
 
 def b64dec(bs: bytes) -> bytes:
     return b64decode(bs)
+
+
+def urlenc(string: Union[str, bytes]) -> str:
+    return quote(string)
+
+
+def urldec(string: str) -> str:
+    return unquote(string)
