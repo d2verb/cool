abd204462d587e4c878e969545b1351c6cefb268
d2verb <licprog73@gmail.com>
2020-07-06 01:19:53 +0900

    add test for urlsafe mode for b64enc, b64dec

--- tests/util/test_enc.py
+++ tests/util/test_enc.py
@@ -4,9 +4,17 @@ from ctftools.util.enc import b64dec, b64enc, urldec, urlenc
 def test_b64enc():
     assert b64enc(b"Lorem Ipsum") == b"TG9yZW0gSXBzdW0="
 
+    # test urlsafe mode
+    assert b64enc(b"\xf8\xff\xff") == b"+P//"
+    assert b64enc(b"\xf8\xff\xff", url=True) == b"-P__"
+
 
 def test_b64dec():
     assert b64dec(b"TG9yZW0gSXBzdW0=") == b"Lorem Ipsum"
+
+    # test urlsafe mode
+    assert b64dec(b"+P//") == b"\xf8\xff\xff"
+    assert b64dec(b"-P__", url=True) == b"\xf8\xff\xff"
 
 
 def test_urlenc():
