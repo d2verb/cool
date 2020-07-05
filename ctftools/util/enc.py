b85a4bce495004691d5a36912fc8cae2d201bb8f
d2verb <licprog73@gmail.com>
2020-07-06 01:09:52 +0900

    add urlsafe mode to b64enc and b64dec

--- ctftools/util/enc.py
+++ ctftools/util/enc.py
@@ -1,16 +1,23 @@
-from base64 import b64decode, b64encode
+from base64 import b64decode, b64encode, urlsafe_b64decode, urlsafe_b64encode
 from typing import Union
 from urllib.parse import quote, unquote
 
 
-def b64enc(bs: bytes) -> bytes:
-    return b64encode(bs)
+def b64enc(bs: bytes, url: bool = False) -> bytes:
+    if url:
+        return urlsafe_b64encode(bs)
+    else:
+        return b64encode(bs)
 
 
-def b64dec(bs: bytes) -> bytes:
+def b64dec(bs: bytes, url: bool = False) -> bytes:
     if len(bs) % 4 != 0:
         bs += b"=" * (len(bs) % 4)
-    return b64decode(bs)
+
+    if url:
+        return urlsafe_b64decode(bs)
+    else:
+        return b64decode(bs)
 
 
 def urlenc(string: Union[str, bytes]) -> str:
